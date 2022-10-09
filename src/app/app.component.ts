f9bfd5a44fe84f13f66bd634cafed0e9677625e7
robe <robe@robe.com>
2022-10-09 12:51:41 -0500

    languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component,ViewChild,ElementRef } from '@angular/core';
+import { Component,ViewChild,ElementRef,OnInit,DoCheck } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
+interface languageOption{
+  symbol:string,
+  languageText:string,
+  language:string
+
+}
 
 @Component({
   selector: 'app-root',
@@ -16,8 +23,16 @@ export class AppComponent {
   public initialWords: number;
   public chartSpeed:number;
   public wordSpeed:number;
+  public esFlags:Array<string>;
+  public deFlags:Array<string>;
+  public enFlags:Array<string>;
+  public languagesFlags: Array<Array<string>>;
+  public languageOptions: Array<languageOption>;
+
   @ViewChild("textElement",{static:false}) textArea:ElementRef;
-  constructor(){
+  constructor(
+    private _translate: TranslateService
+  ){
     this.text = "";    
     this.textArea = {} as ElementRef;
     this.startTime = new Date();
@@ -26,9 +41,48 @@ export class AppComponent {
     this.initialWords = 0;
     this.chartSpeed = 0;
     this.wordSpeed = 0;
+    _translate.setDefaultLang('en');
+    _translate.use('en');
+    this.esFlags = [
+      "🇦🇷","🇨🇱","🇨🇴","🇧🇴","🇨🇷","🇨🇺","🇩🇴","🇪🇨","🇸🇻","🇵🇾","🇬🇳","🇬🇹","🇭🇳","🇲🇽","🇳🇮","🇵🇦","🇵🇪","🇪🇸","🇺🇾","🇻🇪"
+    ];
+    this.enFlags = [
+      "🇬🇧","🇺🇸","🇦🇺","🇦🇬","🇧🇸","🇧🇧","🇧🇿","🇧🇼","🇨🇲","🇨🇦","🇩🇲","🇪🇷","🇵🇭","🇫🇯","🇬🇲","🇬🇭","🇬🇩","🇬🇾","🇮🇳","🇮🇪",
+      "🇯🇲","🇯🇴","🇰🇪","🇰🇮","🇱🇸","🇱🇷","🇲🇼","🇲🇹","🇲🇭","🇲🇺","🇫🇲","🇳🇦","🇳🇷","🇳🇿","🇳🇬","🇵🇰","🇵🇼","🇵🇬","🇷🇼","🇰🇳","🇱🇨","🇻🇨",
+      "🇦🇸","🇸🇨","🇸🇱","🇸🇬","🇸🇧","🇿🇦","🇸🇩","🇸🇿","🇹🇴","🇹🇿","🇹🇹","🇹🇻","🇺🇬","🇻🇺","🇿🇲","🇿🇼","🇼🇸","🇦🇮","🇧🇲","🇰🇾","🇯🇪","🇬🇮","🇳🇺",
+      "🇳🇫","🇵🇷","🇹🇰","🇨🇨","🇫🇰","🇻🇬","🇻🇮","🇬🇺","🇬🇬","🇭🇰","🇮🇲","🇨🇰","🇨🇽","🇲🇸","🇹🇨","🇵🇳","🇲🇵"
+    ]
+
+    this.deFlags = [
+      "🇩🇪","🇦🇹","🇨🇭","🇧🇪","🇱🇺","🇱🇮"
+    ]
+    this.languagesFlags = [];
+    this.languageOptions = [];
   }
 
-  
+  ngOnInit(){
+    this.languagesFlags.push(this.deFlags,this.esFlags,this.enFlags);
+    let languagesTextString = ["Deutsch","Español","English"];
+    let languagesOptions = ["de","es","en"];
+    let setOptionsCounter = 0;
+    this.languagesFlags.forEach( languageFlags => {
+      
+      let selectRdmFlag = (max:number) => {
+        return Math.floor(Math.random() * max);
+      };
+
+      let countFlagsOnLanguage = languageFlags.length;
+      let rdmFlag = selectRdmFlag(countFlagsOnLanguage);
+      this.languageOptions.push({
+
+        symbol: languageFlags[rdmFlag],
+        languageText: languagesTextString[setOptionsCounter],
+        language: languagesOptions[setOptionsCounter]
+
+      });
+      setOptionsCounter++;
+    });
+  }
 
   begin(){
     this.textArea.nativeElement.focus();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+
+
+import { TranslateModule,TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +22,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      defaultLanguage:'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+    }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
